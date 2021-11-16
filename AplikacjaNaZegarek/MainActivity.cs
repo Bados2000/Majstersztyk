d3d44167b092e795c21bf9bf89c745cea34685f9
Bados2000 <mimi2222@vp.pl>
2021-11-16 12:46:24 +0100

    Statystyki

--- AplikacjaNaZegarek/MainActivity.cs
+++ AplikacjaNaZegarek/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.Support.Wearable.Activity;
 
 namespace AplikacjaNaZegarek
 {
-    
+
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : WearableActivity
     {
@@ -16,7 +16,7 @@ namespace AplikacjaNaZegarek
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.s2;
 
             textView = FindViewById<TextView>(Resource.Id.text);
             SetAmbientEnabled();
@@ -24,8 +24,18 @@ namespace AplikacjaNaZegarek
         async void GoToS2(object sender, EventArgs args)
         {
             await.Navigation
+    }
+
+        static void Main(string[] args)
+        {
+            // Get current DateTime. It can be any DateTime object in your code.  
+            DateTime aDate = DateTime.Now;
+
+            // Format Datetime in different formats and display them  
+           
+            Console.WriteLine(aDate.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            Console.ReadKey();
         }
+
     }
 }
-
-
